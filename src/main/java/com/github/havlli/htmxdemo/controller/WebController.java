a105c7916544c8b037d6b0990dade8fa4d5f676a
havlli <havlliczech@gmail.com>
2024-05-23 23:32:20 +0200

    basic demo

--- src/main/java/com/github/havlli/htmxdemo/controller/WebController.java
+++ src/main/java/com/github/havlli/htmxdemo/controller/WebController.java
@@ -1,2 +1,16 @@
-package com.github.havlli.htmxdemo.controller;public class WebController {
+package com.github.havlli.htmxdemo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WebController {
+
+    @PostMapping("/submit")
+    public ResponseEntity<String> submit(@RequestParam String message) {
+        return ResponseEntity.ok(message);
+    }
 }
